6f58c21d73dd33070cef01c9748eafa1ad4ee070
Teddius Maingi <maingi.teddius@thetag.co.ke>
2021-12-20 21:34:56 +0300

    new home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,36 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return 'Agripa';
+        return view('landing.home');
+    }
+
+    public function about()
+    {
+        return view('landing.about');
+    }
+
+    public function shop()
+    {
+        return view('landing.shop');
+    }
+
+    public function cart()
+    {
+        return view('landing.cart');
+    }
+
+    public function checkout()
+    {
+        return view('landing.checkout');
+    }
+
+    public function insights()
+    {
+        return view('landing.insights');
+    }
+
+    public function contact()
+    {
+        return view('landing.contact');
     }
 }
