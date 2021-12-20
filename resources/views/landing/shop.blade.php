df55a65ff758b4c81cf8320fdc2656387579bfdb
Teddius Maingi <maingi.teddius@thetag.co.ke>
2021-12-21 01:03:17 +0300

    home done

--- resources/views/landing/shop.blade.php
+++ resources/views/landing/shop.blade.php
@@ -17,7 +17,49 @@
 
 	<div class="mt-150 mb-150">
 		<div class="container">
-            Shop
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-filters">
+                        <ul>
+                            <li class="active" data-filter="*">All</li>
+                            @foreach ($categories as $category)
+                                <li data-filter=".{{ $category->name }}">{{ $category->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row product-lists">
+                @foreach ($categories as $category)
+                    @foreach ($category->commodities as $commodity)
+                        <div class="col-lg-4 col-md-6 text-center {{ $category->name }}">
+                            <div class="single-product-item">
+                                <div class="product-image">
+                                    <a href="#"><img src="{{ asset('uploads/commodities/commmodity-images/' . $commodity->cover_image) }}" alt=""></a>
+                                </div>
+                                <h3>{{ $commodity->name }}</h3>
+                                <p class="product-price"><span>Per Unit</span> {{ $commodity->price }} </p>
+                                <a href="{{ route('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+                            </div>
+                        </div> 
+                    @endforeach
+                @endforeach
+			</div>
+
+            {{-- <div class="row">
+				<div class="col-lg-12 text-center">
+					<div class="pagination-wrap">
+						<ul>
+							<li><a href="#">Prev</a></li>
+							<li><a href="#">1</a></li>
+							<li><a class="active" href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">Next</a></li>
+						</ul>
+					</div>
+				</div>
+			</div> --}}
         </div>
     </div>
 @endsection
