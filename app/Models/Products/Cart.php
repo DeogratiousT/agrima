cde96d93ce1ce890b058189f1cb76b3c769e0f15
DeogratiousT <deogratioust@gmail.com>
2022-02-21 16:20:02 +0300

    redis init

--- app/Models/Products/Cart.php
+++ app/Models/Products/Cart.php
@@ -2,28 +2,32 @@
 
 namespace App\Models\Products;
 
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 
 class Cart
 {
-    public $items = null;
+    public $user_id;
     public $totalPrice = 0;
     public $totalQuantity = 0;
+    public $items = null;
+
 
     public function __construct()
     {
-        $currentCart = Session::has('cart') ? Session::get('cart') : null;
-        
+        $this->user_id = Auth::id();
+        $currentCart = Redis::exists($this->user_id) ? true : false;
+
         if ($currentCart) {
-            $this->items = $currentCart->items;
-            $this->totalPrice = $currentCart->totalPrice;
-            $this->totalQuantity = $currentCart->totalQuantity;
+            $this->totalPrice = Redis::hget($this->user_id, 'totalPrice');
+            $this->totalQuantity = Redis::hget($this->user_id, 'totalQuantity');
+            $this->items = unserialize(Redis::hget($this->user_id, 'items'));
         }
     }
 
     public function addItem($commodity)
     {
-        $itemToStore = ['item' => $commodity, 'price' => $commodity->price, 'quantity' => 0];
+        $itemToStore = ['slug' => $commodity->slug, 'price' => $commodity->price, 'quantity' => 0];
 
         if ($this->items) {
             if (array_key_exists($commodity->slug, $this->items)) {
@@ -34,7 +38,34 @@ class Cart
         $itemToStore['quantity'] ++;
         $itemToStore['price'] = $commodity->price * $itemToStore['quantity'];
         $this->items[$commodity->slug] = $itemToStore;
+        $this->items= serialize($this->items);
         $this->totalQuantity ++;
         $this->totalPrice += $commodity->price;
+
+        $this->storeItems();
+    }
+
+    public function storeItems()
+    {
+        Redis::hmset($this->user_id, 'totalQuantity', $this->totalQuantity, 'totalPrice', $this->totalPrice, 'items', $this->items);
+    }
+
+    public function getTotalQuantity()
+    {
+        return $this->totalQuantity;
+    }
+
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    public function getItems()
+    {
+        dd($this->user_d = [
+            'totalQuantity' => $this->totalQuantity,
+            'totalPrice' => $this->totalPrice,
+            'items' => $this->items
+        ]);        
     }
 }
